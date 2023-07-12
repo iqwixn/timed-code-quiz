3acaa19e6c0cc53a9ecc5bb9d3acccb7ccd799e0
iqwixn <aleksey.nizhnikov@gmail.com>
2023-07-11 19:10:05 -0700

    updated HTML, CSS, JS. Added timer to JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-var card = document.querySelector('.card-contents')
+var startButton = document.getElementById('start-btn')
+var startTimer = document.getElementById('start-btn')
+var questions = document.getElementById('question')
+
+startTimer.addEventListener('click', timerStart)
+startButton.addEventListener('click', startQuiz)
+
+var secondsLeft = 60
+var timer = document.querySelector('.timer');
+
+
+function timerStart() {  
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timer.textContent = secondsLeft ;
+      if(secondsLeft === 0) {       
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+    }, 1000);
+    
+    
+
+
+}
+
+
+
+function startQuiz() {
+    startButton.classList.add('hide')
+    options.classList.remove('hide')
+
+}
+
+
